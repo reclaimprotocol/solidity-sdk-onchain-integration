c6a3ecb0eb2285e850ae3393b85597052dd99835
Scott Brown <scott@creatoros.co>
2024-05-20 13:30:25 +0200

    fix: minimized contract for hedera

--- contracts/Reclaim.sol
+++ contracts/Reclaim.sol
@@ -10,11 +10,6 @@ import "./lib/Random.sol";
 import "./lib/StringUtils.sol";
 import "./lib/BytesUtils.sol";
 
-// import "hardhat/console.sol";
-
-error Reclaim__GroupAlreadyExists();
-error Reclaim__UserAlreadyMerkelized();
-
 /**
  * Reclaim Beacon contract
  */
@@ -49,10 +44,7 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
 	/** list of all epochs */
 	Epoch[] public epochs;
-
-	/** address of the semaphore contract */
-	address public semaphoreAddress;
-
+	
 	/**
 	 * duration of each epoch.
 	 * is not a hard duration, but useful for
@@ -66,25 +58,9 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 	 * */
 	uint32 public currentEpoch;
 
-	/**
-	 * created groups mapping
-	 * map groupId with true if already created
-	 * */
-	mapping(uint256 => bool) createdGroups;
-
-	mapping(uint256 => mapping(string => bool)) isUserMerkelized;
-
 	event EpochAdded(Epoch epoch);
 
-	event GroupCreated(uint256 indexed groupId, string indexed provider);
-
-	event DappCreated(bytes32 indexed dappId);
-
 	bool internal locked;
-
-	mapping(bytes32 => bool) merkelizedUserParams;
-
-	mapping(bytes32 => uint256) dappIdToExternalNullifier;
 
 	// Modifiers
 	modifier noReentrant() {
@@ -99,11 +75,10 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 	 *
 	 * @dev This acts as a constructor for the upgradeable proxy contract
 	 */
-	function initialize(address _semaphoreAddress) external initializer {
+	function initialize() external initializer {
 		__Ownable_init();
 		epochDurationS = 1 days;
 		currentEpoch = 0;
-		semaphoreAddress = _semaphoreAddress;
 	}
 
 	/**
@@ -178,79 +153,6 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 		return selectedWitnesses;
 	}
 
-	function createDapp(uint256 id) external {
-		bytes32 dappId = keccak256(abi.encodePacked(msg.sender, id));
-		require(dappIdToExternalNullifier[dappId] != id, "Dapp Already Exists");
-		dappIdToExternalNullifier[dappId] = id;
-		emit DappCreated(dappId);
-	}
-
-	/**
-	 * Get the provider name from the proof
-	 */
-	function getProviderFromProof(
-		Proof memory proof
-	) external pure returns (string memory) {
-		return proof.claimInfo.provider;
-	}
-
-	function extractFieldFromContext(
-		string memory data,
-		string memory target
-	) public pure returns (string memory) {
-		bytes memory dataBytes = bytes(data);
-		bytes memory targetBytes = bytes(target);
-
-		require(dataBytes.length >= targetBytes.length, "target is longer than data");
-		uint start = 0;
-		bool foundStart = false;
-		// Find start of "contextMessage":"
-
-		for (uint i = 0; i <= dataBytes.length - targetBytes.length; i++) {
-			bool isMatch = true;
-
-			for (uint j = 0; j < targetBytes.length && isMatch; j++) {
-				if (dataBytes[i + j] != targetBytes[j]) {
-					isMatch = false;
-				}
-			}
-
-			if (isMatch) {
-				start = i + targetBytes.length; // Move start to the end of "contextMessage":"
-				foundStart = true;
-				break;
-			}
-		}
-
-		if (!foundStart) {
-			return ""; // Malformed or missing message
-		}
-		// Find the end of the message, assuming it ends with a quote not preceded by a backslash
-		uint end = start;
-		while (
-			end < dataBytes.length &&
-			!(dataBytes[end] == '"' && dataBytes[end - 1] != "\\")
-		) {
-			end++;
-		}
-		if (end <= start) {
-			return ""; // Malformed or missing message
-		}
-		bytes memory contextMessage = new bytes(end - start);
-		for (uint i = start; i < end; i++) {
-			contextMessage[i - start] = dataBytes[i];
-		}
-		return string(contextMessage);
-	}
-
-	function getMerkelizedUserParams(
-		string memory provider,
-		string memory params
-	) external view returns (bool) {
-		bytes32 userParamsHash = calculateUserParamsHash(provider, params);
-		return merkelizedUserParams[userParamsHash];
-	}
-
 	/**
 	 * Call the function to assert
 	 * the validity of several claims proofs
@@ -297,73 +199,6 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 		// @TODO: verify zkproof
 	}
 
-	function createGroup(
-		string memory provider,
-		uint256 merkleTreeDepth // address admin
-	) public {
-		uint256 groupId = calculateGroupIdFromProvider(provider);
-		if (createdGroups[groupId] == true) {
-			revert Reclaim__GroupAlreadyExists();
-		}
-		SemaphoreInterface(semaphoreAddress).createGroup(
-			groupId,
-			merkleTreeDepth,
-			address(this)
-		);
-		createdGroups[groupId] = true;
-		emit GroupCreated(groupId, provider);
-	}
-
-	function merkelizeUser(
-		Proof memory proof,
-		uint256 _identityCommitment
-	) external noReentrant {
-		uint256 groupId = calculateGroupIdFromProvider(proof.claimInfo.provider);
-		bytes32 userParamsHash = calculateUserParamsHash(
-			proof.claimInfo.provider,
-			proof.claimInfo.parameters
-		);
-		if (merkelizedUserParams[userParamsHash] == true) {
-			revert Reclaim__UserAlreadyMerkelized();
-		}
-		verifyProof(proof);
-		if (createdGroups[groupId] != true) {
-			createGroup(proof.claimInfo.provider, 20);
-		}
-		SemaphoreInterface(semaphoreAddress).addMember(groupId, _identityCommitment);
-		merkelizedUserParams[userParamsHash] = true;
-	}
-
-	function verifyMerkelIdentity(
-		string memory provider,
-		uint256 _merkleTreeRoot,
-		uint256 _signal,
-		uint256 _nullifierHash,
-		uint256 _externalNullifier,
-		bytes32 dappId,
-		uint256[8] calldata _proof
-	) external returns (bool) {
-		require(
-			dappIdToExternalNullifier[dappId] == _externalNullifier,
-			"Dapp Not Created"
-		);
-		uint256 groupId = calculateGroupIdFromProvider(provider);
-		try
-			SemaphoreInterface(semaphoreAddress).verifyProof(
-				groupId,
-				_merkleTreeRoot,
-				_signal,
-				_nullifierHash,
-				_externalNullifier,
-				_proof
-			)
-		{
-			return true;
-		} catch {
-			return false;
-		}
-	}
-
 	// admin functions ---
 
 	/**
@@ -392,38 +227,5 @@ contract Reclaim is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 		}
 
 		emit EpochAdded(epochs[epochs.length - 1]);
-	}
-
-	// internal code -----
-
-	function uintDifference(uint256 a, uint256 b) internal pure returns (uint256) {
-		if (a > b) {
-			return a - b;
-		}
-
-		return b - a;
-	}
-
-	/**
-	 * @dev Get/Calculate the groupId for a specific provider
-	 */
-	function calculateGroupIdFromProvider(
-		string memory provider
-	) internal pure returns (uint256) {
-		bytes memory providerBytes = bytes(provider);
-		bytes memory hashedProvider = abi.encodePacked(keccak256(providerBytes));
-		uint256 groupId = BytesUtils.bytesToUInt(
-			hashedProvider,
-			hashedProvider.length - 4
-		);
-		return groupId;
-	}
-
-	function calculateUserParamsHash(
-		string memory provider,
-		string memory params
-	) internal pure returns (bytes32) {
-		bytes32 userParamsHash = keccak256(abi.encodePacked(provider, params));
-		return userParamsHash;
 	}
 }
--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,6 +1,5 @@
 import { task } from 'hardhat/config'
 import verify from '../scripts/verify'
-import { ReturnObjectSemaphoreDeployTask } from '../types'
 import fs from 'fs'
 
 task('deploy').setAction(async ({}, { ethers, network, upgrades }) => {
@@ -9,19 +8,10 @@ task('deploy').setAction(async ({}, { ethers, network, upgrades }) => {
   )
   const networkDetails = content['networks'][network.name]
 
-  const {
-    semaphore,
-    pairingAddress,
-    semaphoreVerifierAddress,
-    poseidonAddress,
-    incrementalBinaryTreeAddress
-  } = // @ts-expect-error events
-    (await run('deploy:semaphore')) as ReturnObjectSemaphoreDeployTask
-
   const ReclaimFactory = await ethers.getContractFactory('Reclaim')
   const Reclaim = await upgrades.deployProxy(
     ReclaimFactory,
-    [semaphore.address],
+    [],
     {
       kind: 'uups',
       initializer: 'initialize'
@@ -30,29 +20,10 @@ task('deploy').setAction(async ({}, { ethers, network, upgrades }) => {
   const tx = await Reclaim.deployed()
   const res = await tx.deployTransaction.wait()
 
-  // @ts-expect-error events
   console.log('Reclaim Implementation deployed to:', res.events[0].args[0])
   console.log('Reclaim Proxy deployed to: ', Reclaim.address)
 
   
-  networkDetails['IncrementalBinaryTree'] = {
-    address: incrementalBinaryTreeAddress,
-    explorer: ''
-  }
-
-  networkDetails['Pairing'] = {
-    address: pairingAddress,
-    explorer: ''
-  }
-  networkDetails['SemaphoreVerifier'] = {
-    address: semaphoreVerifierAddress,
-    explorer: ''
-  }
-  networkDetails['Semaphore'] = {
-    address: semaphore.address,
-    explorer: ''
-  }
-
   networkDetails['Reclaim'] = {
     address: Reclaim.address,
     explorer: ''
@@ -64,10 +35,5 @@ task('deploy').setAction(async ({}, { ethers, network, upgrades }) => {
     JSON.stringify(content)
   )
 
-
-  await verify(incrementalBinaryTreeAddress, network.name)
-  await verify(pairingAddress, network.name)
-  await verify(semaphoreVerifierAddress, network.name)
-  await verify(semaphore.address, network.name, [semaphoreVerifierAddress])
-  await verify(Reclaim.address, network.name, [semaphore.address])
+  await verify(Reclaim.address, network.name, [])
 })

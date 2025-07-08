04a87971b7240ac805c86af36e6fba3fd3f9908b
Hadi Saleh <89351369+TheLedgerOfJoudi@users.noreply.github.com>
2025-07-08 21:05:54 +0300

    Merge pull request #3 from reclaimprotocol/dependabot/npm_and_yarn/crypto-sdk/multi-6c1573a11c
    chore(deps): bump elliptic and ethers in /crypto-sdk

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,5 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -249,4 +251,4 @@ const config: HardhatUserConfig = {
   }
 }
 
-export default config
+export default config;
